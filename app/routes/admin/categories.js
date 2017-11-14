b5895e56e5fc9840a04a78cac2514b6fd414eec6
elliot richardson <ear78@aol.com>
2017-11-14 00:03:58 -0600

    added admin categories route and tested model hook for data

---
+++ app/routes/admin/categories.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+    model() {
+    return [
+      {
+        id: 1,
+        name: 'First Category'
+      },
+      {
+        id: 2,
+        name: 'Second Category'
+      }
+    ];
+  }
+});
